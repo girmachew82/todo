efc70c4dc85eb0c5e0c052d799b3782e71473ab7
girmachew82 <girmachewabebe@gmail.com>
2025-08-07 21:41:31 +0300

    todo

--- server/todos/models.py
+++ server/todos/models.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    completed = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-    
-    
